4b33e079de318866a6ad2475e2cf6232ee5e42a1
victorcapelini <victorcapelini@hotmail.com>
2018-09-19 22:26:32 -0300

    Commit inicial;

---
+++ VictorCapelini19092018/VictorCapelini19092018/Controllers/EmpresaController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VictorCapelini19092018.Models;
+
+namespace VictorCapelini19092018.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        IList<Empresa> empresaTeste = new List<Empresa>();
+        Empresa empresa = new Empresa("testenome", "2133412312", "testers");
+
+
+
+        // GET: Empresa
+        public ActionResult Index()
+        {
+            empresaTeste.Add(empresa);
+            return View(empresaTeste);
+        }
+
+        // GET: Empresa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Empresa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empresa/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Empresa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Empresa/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Empresa/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Empresa/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ VictorCapelini19092018/VictorCapelini19092018/Models/Empresa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VictorCapelini19092018.Models
+{
+    public class Empresa
+    {
+
+        public string Nome { get; private set; }
+        public string CNPJ { get; private set; }
+        public DateTime DataDeCadastro { get; private set; }
+        public string RazaoSocial { get; private set; }
+
+        public Empresa(string nome, string cNPJ, string razaoSocial)
+        {
+            Nome = nome;
+            CNPJ = cNPJ;
+            DataDeCadastro = DateTime.Now;
+            RazaoSocial = razaoSocial;
+        }
+
+
+
+    }
+}
